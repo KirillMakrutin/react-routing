d23252fb5d0cf2474c85803c534d0b3039401740
Kirill Makrutin <k.makrutin@gmail.com>
2018-04-22 09:28:39 +0300

    198

--- src/containers/Blog/NewPost/NewPost.js
+++ src/containers/Blog/NewPost/NewPost.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react';
 import axios from 'axios';
+import { Redirect } from 'react-router-dom';
 
 import './NewPost.css';
 
@@ -7,8 +8,9 @@ class NewPost extends Component {
     state = {
         title: '',
         content: '',
-        author: 'Max'
-    }
+        author: 'Max',
+        submitted: false
+    };
 
     postDataHandler = () => {
         const data = {
@@ -19,6 +21,7 @@ class NewPost extends Component {
         axios.post('/posts', data)
             .then(response => {
                 console.log(response);
+                this.setState({submitted: true})
             });
     };
 
@@ -27,8 +30,16 @@ class NewPost extends Component {
     }
 
     render () {
+        let redirect = null;
+
+        if( this.state.submitted )
+        {
+            redirect = <Redirect to="/posts"/>
+        }
+
         return (
             <div className="NewPost">
+                {redirect}
                 <h1>Add a Post</h1>
                 <label>Title</label>
                 <input type="text" value={this.state.title} onChange={(event) => this.setState({title: event.target.value})} />
